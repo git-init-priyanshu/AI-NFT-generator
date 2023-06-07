06cac6e8521ba2d236ff41adc6e85c9d26df66ca
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-06-07 11:21:29 +0530

    Added feature to connect to metamask

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 import "./App.css";
 import InputPrompt from "./Components/InputPrompt";
+import SaveNftToMetamask from "./Components/SaveNftToMetamask";
 
 function App() {
   return (
     <>
       <InputPrompt />
+      <SaveNftToMetamask />
     </>
   );
 }
