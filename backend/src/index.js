5647b802d640f99f1ab709d4f8facb194f961553
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-06-25 14:57:24 +0530

    Integrating backend

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+// const port = 5000;
+const app = express();
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+// Available Routes
+app.use("/api", require("../routes/getImage_StableDiffusion"));
+app.use("/api", require("../routes/storeToIpfs_Pinata"));
+app.use("/api", require("../routes/getData"));
+
+module.exports.handler = serverless(app);
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
