5e3590ea9a7ddeee6e5dce2077045f48f6b6db94
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-06-25 15:02:35 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ const cors = require("cors");
 app.use(cors());
 
 // Available Routes
-app.use("/api", require("../routes/getImage_StableDiffusion"));
-app.use("/api", require("../routes/storeToIpfs_Pinata"));
-app.use("/api", require("../routes/getData"));
+app.use("/api", require("./routes/getImage_StableDiffusion"));
+app.use("/api", require("./routes/storeToIpfs_Pinata"));
+app.use("/api", require("./routes/getData"));
 
 module.exports.handler = serverless(app);
 
