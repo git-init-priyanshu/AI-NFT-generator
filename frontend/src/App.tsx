c534043f69774390fa025b3c8ed4c665173b0728
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-07-10 19:24:04 +0530

    Initialized Apollo Graphql in frontend, Upgraded react-router-dom to v6.14.1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,55 @@
-// import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
 import GenerateNFT from "./Pages/GenerateNFT";
 import ShowMyNFTs from "./Pages/ShowMyNFTs";
 import { NFTprovider } from "./Components/context/NFTcontext";
 import StableDiffusionAPI from "./Pages/StableDiff_API";
+import "./App.css";
+
+// Defining router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<StableDiffusionAPI />} />
+      <Route path="/generateNFT" element={<GenerateNFT />} />
+      <Route path="/showMyNFTs" element={<ShowMyNFTs />} />
+    </Route>
+  )
+);
 
 const App = () => {
+  // Defining Apollo Client
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/",
+  });
+
   return (
     <div className=" h-screen mx-10">
-      <NFTprovider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<StableDiffusionAPI />} />
-            <Route path="generateNFT" element={<GenerateNFT />} />
-            <Route path="showMyNFTs" element={<ShowMyNFTs />} />
-          </Routes>
-        </Router>
-      </NFTprovider>
+      <ApolloProvider client={client}>
+        <NFTprovider>
+          <RouterProvider router={router} />
+        </NFTprovider>
+      </ApolloProvider>
     </div>
   );
 };
+
+function Root() {
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
 
 export default App;
